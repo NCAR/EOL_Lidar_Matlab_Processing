850f86fdd8eab925d0ee1c85f5ff21eee0ebf9c7
sspuler <spuler@ucar.edu>
2020-03-19 15:10:55 -0600

    more efficient checking which node is being called and lableing the plots with node

--- DIAL_multiday_plots_NetCDF_function.m
+++ DIAL_multiday_plots_NetCDF_function.m
@@ -1,79 +1,8 @@
 function[] = DIAL_multiday_plots_NetCDF_function(save_figs, save_data, near, afterpulse, node, daystr, daystr2)
 %clear all; 
 close all;
-dd = pwd; % get the current path
-
 tic
-
-%DIAL=1;
-%date = '01 Jul 2014'; % FRAPPE (no WS)
-%days = 50; skip = 5; % days to plot and days to skip ticks for plots
-%date = '27 May 2015'; %PECAN (no WS)
-%days =50; skip = 5; % days to plot and days to skip ticks for plots ** 'skip' must evenly divide into 'days'
-%date = '24 Apr 2017'; % Perdigao 
-%days = 55; skip = 5;
-%date = '15 May 2017'; % Perdigao 
-%days = 3; skip = 1;
-%date = '24 May 2017'; % Perdigao 
-%days = 10; skip = 2;
-%date = '26 Oct 2018'; % RELAMPAGO high data rate  
-%days = 52; skip = 4;
-%date = '16 Nov 2018'; % RELAMPAGO low data rate
-%days = 24; skip = 2;
-%date = '01 Dec 2018'; % RELAMPAGO high data rate  
-%$days = 17; skip = 2;
-%date = '05 Apr 2019'; % Five-unit side-by-side test  
-%days = 5; skip = 1;
-
-%DIAL=2;
-%date = '28 Jul 2017'; % DLB-HSRL and WV-DIAL @ LAFE 
-%days = 40; skip = 4;
-%date = '14 Aug 2017'; % DLB-HSRL and WV-DIAL @ LAFE 
-%days = 3; skip = 1;
-%date = '12 Oct 2017'; % DLB-HSRL and WV-DIAL @ LAFE 
-%days = 21; skip = 3;
-%date = '03 Jan 2018'; % DLB-HSRL and WV-DIAL post LAFE finalizing rack mounting
-%days = 21; skip = 3;
-%date = '05 Aug 2017'; % Perdigao 
-%days = 10; skip = 2;
-%date = '05 Apr 2019'; % Five-unit side-by-side test  
-%days = 5; skip = 1;
-
-%DIAL=3;
-%date = '11 Jun 2018'; %  
-%days = 176; skip = 11;  % shoot for about 14 ticks
-%date = '05 Aug 2018'; %  
-%days = 60; skip = 5;  % shoot for about 14 ticks
-%date = '04 Oct 2018'; %  
-%days = 60; skip = 5;  % shoot for about 14 ticks
-%date = '05 Apr 2019'; % Five-unit side-by-side test  
-%days = 5; skip = 1;
-
-%DIAL=4;
-%date = '08 Aug 2018'; %  
-%days = 184; skip = 16;  % shoot for about 14 ticks
-%date = '08 Aug 2018'; %  
-%days = 80; skip = 5;  % shoot for about 14 ticks
-%date = '07 Oct 2018'; %  
-%days = 60; skip = 5;  % shoot for about 14 ticks
-%date = '05 Apr 2019'; % Five-unit side-by-side test  
-%days = 5; skip = 1;
-
-%DIAL=5;
-%date = '17 Apr 2019'; % SGP test  
-%days = 7; skip = 1;
-%days = 32; skip = 4;
-%days = 72; skip = 9;
-%days = 96; skip = 12;
-
-%DIAL=4;
-%date = '16 Aug 2019'; % Comparison test new butterfly DBR + TWA transmitter 
-%days = 55; skip = 5;
-
-%DIAL=node;
-%date = '16 Dec 2019'; % data processed to 13-Dec (available to 7-Dec 2019)
-%days = 85; skip = 5;
-%date = '04 Mar 2020'; % data processed to 13-Dec (available to 7-Dec 2019)
+dd = pwd; % get the current path
 
 date = datestr(datenum(daystr,'yyyymmdd'), 'dd mmm yyyy');
 days = (datenum(daystr2,'yyyymmdd') - datenum(daystr,'yyyymmdd'))+1;
@@ -82,7 +11,8 @@ skip = 2;
 flag.near = near;  % read in the near range channel (0=off 1=on)
 flag.afterpulse = afterpulse; % read in the afterpulse corrected data (0=off 1=on)
 
-font_size = 36; % use this for 2018a version
+font_size = 28; % use this for 2019b version
+%font_size = 36; % use this for 2018a version
 %font_size = 14; % use this for 2015a version
 %font_size = 16; % use this for 2015a versionexit
 %font_size = 28; % use this for 2014a version
@@ -106,21 +36,8 @@ range_grid_size = 75;
 
 
 RB_scale = 1;
-%number of accumulations 
-%if datenum(date)>=datenum('16 Nov 2018')&& datenum(date)<=datenum('18 Dec 2018')
-%  accum = 32000; %  
-%  bin_duration = 500;  % ns 
-%  RB_scale = 1; % use to keep the arbitrary units of RB scale the same before
-%else
-%  accum = 14000; %this was changed before perdigao need to get the date
-%  bin_duration = 250;  % ns (this change from 50 to 500 on 14-June-2014)
-%  RB_scale = 1;
-%end
-  
 C = importdata('NCAR_C_Map.mat');
-%cd('/Users/spuler/Desktop/FRAPPE_PECAN') % point to the directory where data is stored 
-%cd('/Users/spuler/Desktop/WV_DIAL_data/') % point to the directory where data is stored 
-dd=pwd;
+
 
 if strcmp(getenv('HOSTNAME'),'fog.eol.ucar.edu')
    serv_path = '/export/fog1/rsfdata/MPD/'; % when running on server
@@ -128,25 +45,30 @@ else
    serv_path = '/Volumes/eol/fog1/rsfdata/MPD/'; % 
 end
 
+
 if strcmp(node,'MPD1')==1
     cd(strcat(serv_path, 'wvdial_1_processed_data/Matlab')) % point to the directory where data is stored 
-    %bin duration in ns
+    plot_dir = (strcat(serv_path, 'wvdial_1_processed_data/Plots'));
   elseif strcmp(node,'MPD2')==1
-    %cd('/Volumes/documents/WV_DIAL_data/MPD2_processed_data') % point to the directory where data is stored 
-    cd(strcat(serv_path, 'wvdial_2_processed_data/Matlab')) 
+    cd(strcat(serv_path, 'wvdial_2_processed_data/Matlab'))
+    plot_dir = (strcat(serv_path, 'wvdial_2_processed_data/Plots'));
   elseif strcmp(node,'MPD3')==1
     %cd('/Volumes/documents/WV_DIAL_data/MPD3_processed_data') % point to the directory where data is stored 
     cd(strcat(serv_path, 'wvdial_3_processed_data/Matlab'))
+    plot_dir = (strcat(serv_path, 'wvdial_3_processed_data/Plots'));
     if flag.afterpulse == 1
       %cd('/Volumes/documents/WV_DIAL_data/MPD3_processed_data_afterpulse_corrected') % point to the directory where data is stored   
       cd(strcat(serv_path, 'wvdial_3_processed_data/Matlab/afterpulse'))
+      plot_dir = (strcat(serv_path, 'wvdial_3_processed_data/Matlab/afterpulse'));
     end
   elseif strcmp(node,'MPD4')==1
     %cd('/Volumes/documents/WV_DIAL_data/MPD4_processed_data') % point to the directory where data is stored 
-    cd(strcat(serv_path, 'wvdial_4_processed_data/Matlab'))   
+    cd(strcat(serv_path, 'wvdial_4_processed_data/Matlab'))
+    plot_dir = (strcat(serv_path, 'wvdial_4_processed_data/Plots'));
   elseif strcmp(node,'MPD5')==1
     %cd('/Volumes/documents/WV_DIAL_data/MPD5_processed_data') % point to the directory where data is stored 
-    cd(strcat(serv_path, 'wvdial_5_processed_data/Matlab')) 
+    cd(strcat(serv_path, 'wvdial_5_processed_data/Matlab'))
+    plot_dir = (strcat(serv_path, 'wvdial_5_processed_data/Plots'));
 end
 
 
@@ -347,10 +269,10 @@ if flag.replot==1
  if days == 1
    datetick('x','HH:MM','keeplimits', 'keepticks');
    xlabel('Time (UTC)','fontweight','b','fontsize',font_size);
-   hh = title({[date,'DIAL Water Vapor (g m^{-3})']},'fontweight','b','fontsize',font_size);
+   hh = title({[date, node, ' Water Vapor (g m^{-3})']},'fontweight','b','fontsize',font_size);
  else
    datetick('x','dd-mmm-yy','keeplimits', 'keepticks');
-   hh = title({'DIAL Water Vapor (g m^{-3})'},'fontweight','b','fontsize',font_size);
+   hh = title({[node, ' Water Vapor (g m^{-3})']},'fontweight','b','fontsize',font_size);
  end
  set(gca,'Fontsize',font_size,'Fontweight','b');
  
@@ -381,11 +303,11 @@ if flag.replot==1
    datetick('x','HH:MM','keeplimits', 'keepticks');
    xlabel('Time (UTC)','fontweight','b','fontsize',font_size);
   % hh = title({[date,'DIAL Relative Backscatter (C/ns km^2)']},'fontweight','b','fontsize',font_size);
-   hh = title({[date,'DIAL Attenuated Backscatter (A.U.)']},'fontweight','b','fontsize',font_size);
+   hh = title({[date, node, ' Attenuated Backscatter (A.U.)']},'fontweight','b','fontsize',font_size);
  else
    datetick('x','dd-mmm-yy','keeplimits', 'keepticks');
  %  hh = title({'DIAL Relative Backscatter (C/ns km^2)'},'fontweight','b','fontsize',font_size);
-    hh = title({'DIAL Attenuated Backscatter (A.U.)'},'fontweight','b','fontsize',font_size);
+    hh = title({[node, ' Attenuated Backscatter (A.U.)']},'fontweight','b','fontsize',font_size);
  end
  set(gca,'Fontsize',font_size,'Fontweight','b');
  set(gca,'Zscale', 'log')
@@ -423,10 +345,10 @@ if flag.replot==1
   if days == 1
     datetick('x','HH:MM','keeplimits', 'keepticks');
     xlabel('Time (UTC)','fontweight','b','fontsize',font_size); 
-    hh = title({[date,'Background']},'fontweight','b','fontsize',font_size);
+    hh = title({[date, node, ' Background']},'fontweight','b','fontsize',font_size);
   else
     datetick('x','dd-mmm-yy','keeplimits', 'keepticks');
-    hh = title({'DIAL Background'},'fontweight','b','fontsize',font_size); 
+    hh = title({[node, ' Background']},'fontweight','b','fontsize',font_size); 
   end
   set(gca,'Fontsize',font_size,'Fontweight','b');
   
@@ -578,27 +500,9 @@ end
 %% 
 if flag.save_figs==1
   %cd('/Users/spuler/Desktop/WV_DIAL_data/plots/') % point to the directory where data is stored 
-  %cd('/Volumes/documents/WV_DIAL_data/plots/') % point to the directory where data is stored 
-  if strcmp(node,'MPD1')==1
-    cd(strcat(serv_path, 'wvdial_1_processed_data/Plots')) % point to the directory where data is stored 
-    %bin duration in ns
-  elseif strcmp(node,'MPD2')==1
-    %cd('/Volumes/documents/WV_DIAL_data/MPD2_processed_data') % point to the directory where data is stored 
-    cd(strcat(serv_path, 'wvdial_2_processed_data/Plots')) 
-  elseif strcmp(node,'MPD3')==1
-    %cd('/Volumes/documents/WV_DIAL_data/MPD3_processed_data') % point to the directory where data is stored 
-    cd(strcat(serv_path, 'wvdial_3_processed_data/Plots'))
-    if flag.afterpulse == 1
-      %cd('/Volumes/documents/WV_DIAL_data/MPD3_processed_data_afterpulse_corrected') % point to the directory where data is stored   
-      cd(strcat(serv_path, 'wvdial_3_processed_data/Matlab/afterpulse'))
-    end
-  elseif strcmp(node,'MPD4')==1
-    %cd('/Volumes/documents/WV_DIAL_data/MPD4_processed_data') % point to the directory where data is stored 
-    cd(strcat(serv_path, 'wvdial_4_processed_data/Plots'))   
-  elseif strcmp(node,'MPD5')==1
-    %cd('/Volumes/documents/WV_DIAL_data/MPD5_processed_data') % point to the directory where data is stored 
-    cd(strcat(serv_path, 'wvdial_5_processed_data/Plots')) 
-end
+  %cd('/Volumes/documents/WV_DIAL_data/plots/') % point to the directory where data is stored
+  cd(plot_dir)
+    
   date=datestr(nanmean(time_new), 'yyyymmdd');
   
   %Scrnsize = [scrsz(4)/2 scrsz(4)/10 scrsz(3)/1 scrsz(4)/2]; % use for standard plots
