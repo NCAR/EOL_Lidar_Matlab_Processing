3169add0b7a8aecb6925b36ece3831f6de37019a
sspuler <spuler@ucar.edu>
2020-05-19 16:49:08 -0600

    turn on decimation to run test

--- MPD_batch_process_v2.m
+++ MPD_batch_process_v2.m
@@ -7,7 +7,7 @@
 start_day = datenum(start_date,'yyyymmdd');
 stop_day = datenum(stop_date,'yyyymmdd');
 k=start_day;
-flag.process = 0;
+flag.process = 1;
 flag.plot = 1;
 
 for n=1:1
@@ -16,8 +16,8 @@ tStart = tic
       for k=start_day:stop_day
         file = datestr(k, 'yyyymmdd');  
         % save_quicklook, save_data, save_netCDF, save_catalog, channels, correction, node, daystr 
-        MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_ON','MPD04',file) % MPD 04 
-        MPD_process_NetCDF_function_v2(0,1,0,0,'ALL','AP_ON','MPD03',file) % MPD 03 
+        MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD04',file) % MPD 04 
+        MPD_process_NetCDF_function_v2(0,1,0,0,'ALL','AP_OFF','MPD03',file) % MPD 03 
         
 %         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD01',file) 
 %         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD02',file) 
@@ -42,13 +42,13 @@ tStart = tic
 %     MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD04', start_date, stop_date) 
 %     MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD05', start_date, stop_date)        
       
-      MPD_multiday_plots_NetCDF_function(1,0,0,0,'MPD04', start_date, stop_date) % MPD 04 (high range) 
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD04', start_date, stop_date) % MPD 04 (high range) 
 %      MPD_multiday_plots_NetCDF_function(1,0,1,0,'MPD04', start_date, stop_date) % MPD 04 (low range)
 %      MPD_multiday_plots_NetCDF_function(1,0,0,1,'MPD04', start_date, stop_date) % MPD 04 (high range with afterpulse) 
 %      MPD_multiday_plots_NetCDF_function(1,0,1,1,'MPD04', start_date, stop_date) % MPD 04 (low range with afterpulse)  
-      MPD_multiday_plots_NetCDF_function(1,0,0,0,'MPD03', start_date, stop_date) % MPD 03 (high gain)
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD03', start_date, stop_date) % MPD 03 (high gain)
 %      MPD_multiday_plots_NetCDF_function(1,0,1,0,'MPD03', start_date, stop_date) % MPD 03 (low gain)
-      MPD_multiday_plots_NetCDF_function(1,0,0,1,'MPD03', start_date, stop_date) % MPD 03 (high gain with afterpulse)
+%      MPD_multiday_plots_NetCDF_function(1,0,0,1,'MPD03', start_date, stop_date) % MPD 03 (high gain with afterpulse)
 %      MPD_multiday_plots_NetCDF_function(1,0,1,1,'MPD03', start_date, stop_date) % MPD 03 (low gain with afterpulse)
     end
 tElapsed = toc(tStart) 
