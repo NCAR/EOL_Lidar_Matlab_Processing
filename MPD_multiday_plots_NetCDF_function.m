53a780b7b86b096847abe52efff5faf768ee8030
sspuler <spuler@ucar.edu>
2020-05-18 22:17:31 -0600

    Update MPD_multiday_plots_NetCDF_function.m
    range

--- MPD_multiday_plots_NetCDF_function.m
+++ MPD_multiday_plots_NetCDF_function.m
@@ -109,6 +109,7 @@ for i=1:days
        %  OD = interp1(range, OD', range_grid_75, 'linear', 'extrap')';
          range = range_grid_75;
  %        range_limit = range_limit_N_avg/2
+         size(range) 
      end
     N_avg_comb=N_avg_grid;
     RB_comb=RB_grid;
@@ -152,8 +153,9 @@ for i=1:days
   %       OD = interp1(range, OD', range_grid_75, 'linear', 'extrap')';
          range = range_grid_75;
   %       range_limit = range_limit_N_avg2/2
+         size(range)   
       end
-    range_lim1 = size(N_avg_comb,1) % catch any changes in range
+    range_lim1 = size(N_avg_comb,2) % catch any changes in range
     range_lim2 = size(N_avg,2) % catch any changes in range
     range_limit = min([range_lim1 range_lim2])
     
