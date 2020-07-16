d625f186808de1ab684c771b06402a5f0a9340ca
Spuler <spuler@ucar.edu>
2020-07-16 12:38:20 -0600

    updating the lowest gate info and processing MPD01 now

--- MPD_batch_process_v2.m
+++ MPD_batch_process_v2.m
@@ -1,6 +1,6 @@
 % example batch process
- start_date = '20200626';
- stop_date = '20200626';
+ start_date = '20200714';
+ stop_date = '20200716';
 % start_date = '20190405';
 % stop_date = '20190410';
 
@@ -18,6 +18,7 @@ tStart = tic
         % save_quicklook, save_data, save_netCDF, save_catalog, channels, correction, node, daystr 
         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD04',file) % MPD 04 
         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD03',file) % MPD 03 
+        MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD01',file) % MPD 01 
         
 %         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD01',file) 
 %         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD02',file) 
@@ -29,8 +30,8 @@ tStart = tic
     end
 
 %  start_date = '20200506';
-  start_date = '20200613';
-  stop_date = '20200626';
+  start_date = '20200707';
+  stop_date = '20200714';
 %      start_date = '20190405';
 %      stop_date = '20190410';
 
@@ -51,6 +52,7 @@ tStart = tic
 %      MPD_multiday_plots_NetCDF_function(1,0,1,0,'MPD03', start_date, stop_date) % MPD 03 (low gain)
 %      MPD_multiday_plots_NetCDF_function(1,0,0,1,'MPD03', start_date, stop_date) % MPD 03 (high gain with afterpulse)
 %      MPD_multiday_plots_NetCDF_function(1,0,1,1,'MPD03', start_date, stop_date) % MPD 03 (low gain with afterpulse)
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD01', start_date, stop_date) % MPD 01
     end
 tElapsed = toc(tStart) 
 end
