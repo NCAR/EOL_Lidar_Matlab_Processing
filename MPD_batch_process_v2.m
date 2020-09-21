57f8efe190492e9d651439eff0e1a2fd2bd880d1
sspuler <spuler@ucar.edu>
2020-09-21 12:29:24 -0600

    fix missing thermocouple

--- MPD_batch_process_v2.m
+++ MPD_batch_process_v2.m
@@ -1,12 +1,12 @@
 % example batch process
- start_date = '20200828';
- stop_date = '20200828';
+ start_date = '20200918';
+ stop_date = '20200918';
 
 start_day = datenum(start_date,'yyyymmdd');
 stop_day = datenum(stop_date,'yyyymmdd');
 k=start_day;
 flag.process = 1;
-flag.plot = 1;
+flag.plot = 0;
 
 for n=1:1
 tStart = tic   
@@ -14,16 +14,16 @@ tStart = tic
       for k=start_day:stop_day
         file = datestr(k, 'yyyymmdd');  
         % save_quicklook, save_data, save_netCDF, save_catalog, channels, correction, node, daystr 
-         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD01',file) 
-         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD02',file) 
+ %        MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD01',file) 
+ %        MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD02',file) 
          MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD03',file) 
-         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD04',file)  
-         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD05',file)         
+ %        MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD04',file)  
+ %        MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD05',file)         
       end
     end
 
   start_date = '20200826';
-  stop_date = '20200828';
+  stop_date = '20200921';
 
     if flag.plot == 1
       % save_figs, save_data, near/low, afterpulse, node, daystr, daystr2
