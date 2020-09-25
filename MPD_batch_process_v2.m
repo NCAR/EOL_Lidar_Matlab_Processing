23a8542b64d8a84ac1d63699f0b06f1d9fef2a48
Spuler <spuler@ucar.edu>
2020-09-25 10:08:28 -0600

    Updated time

--- MPD_batch_process_v2.m
+++ MPD_batch_process_v2.m
@@ -1,6 +1,6 @@
 % example batch process
- start_date = '20200923';
- stop_date = '20200923';
+ start_date = '20200924';
+ stop_date = '20200925';
 
 start_day = datenum(start_date,'yyyymmdd');
 stop_day = datenum(stop_date,'yyyymmdd');
@@ -14,23 +14,23 @@ tStart = tic
       for k=start_day:stop_day
         file = datestr(k, 'yyyymmdd');  
         % save_quicklook, save_data, save_netCDF, save_catalog, channels, correction, node, daystr 
-%         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD01',file) 
-%         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD02',file) 
-%         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD03',file) 
-%         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD04',file)  
+         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD01',file) 
+         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD02',file) 
+         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD03',file) 
+         MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD04',file)  
          MPD_process_NetCDF_function_v2(0,1,0,0,'WV','AP_OFF','MPD05',file)         
       end
     end
 
-  start_date = '20200911';
-  stop_date = '20200924';
+  start_date = '20200912';
+  stop_date = '20200925';
 
     if flag.plot == 1
       % save_figs, save_data, near/low, afterpulse, node, daystr, daystr2
-%      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD01', start_date, stop_date) 
-%      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD02', start_date, stop_date) 
-%      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD03', start_date, stop_date) 
-%      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD04', start_date, stop_date) 
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD01', start_date, stop_date) 
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD02', start_date, stop_date) 
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD03', start_date, stop_date) 
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD04', start_date, stop_date) 
       MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD05', start_date, stop_date) 
     end
 tElapsed = toc(tStart) 
