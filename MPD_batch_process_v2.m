be07b9b91126188a67647ef5d88f8c9ecbbb537c
sspuler <spuler@ucar.edu>
2020-10-12 08:58:20 -0600

    update dates

--- MPD_batch_process_v2.m
+++ MPD_batch_process_v2.m
@@ -1,7 +1,7 @@
 % example batch process
 
- start_date = '20200929';
- stop_date = '20201005';
+ start_date = '20201012';
+ stop_date = '20201012';
 
 start_day = datenum(start_date,'yyyymmdd');
 stop_day = datenum(stop_date,'yyyymmdd');
@@ -24,16 +24,16 @@ tStart = tic
     end
 
   start_date = '20200929';
-  stop_date = '20201005';
+  stop_date = '20201012';
 
     if flag.plot == 1
       % save_figs, save_data, near/low, afterpulse, node, daystr, daystr2,
       % skip(tick marks at every day 1, other day, 2 etc)
-      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD01', start_date, stop_date, 1) 
-      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD02', start_date, stop_date, 1) 
-      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD03', start_date, stop_date, 1) 
-      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD04', start_date, stop_date, 1) 
-      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD05', start_date, stop_date, 1) 
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD01', start_date, stop_date, 2) 
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD02', start_date, stop_date, 2) 
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD03', start_date, stop_date, 2) 
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD04', start_date, stop_date, 2) 
+      MPD_multiday_plots_NetCDF_function(1,1,0,0,'MPD05', start_date, stop_date, 2) 
     end
 tElapsed = toc(tStart) 
 end
--- MPD_compare_with_each_other.m
+++ MPD_compare_with_each_other.m
@@ -2,7 +2,7 @@ clear all; close all;
 
 dd = pwd; % get the current path
 %date = '10 Apr 2019'; % Last day of a five-unit side-by-side test  
-date = '05 Oct 2020'; %   
+date = '12 Oct 2020'; %   
 
 
 if strcmp(getenv('HOSTNAME'),'fog.eol.ucar.edu')
